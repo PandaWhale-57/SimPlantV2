83afa37953b517d3adede31d9bb475dd5af8f889
Jessica <vo.jessicam@gmail.com>
2023-04-27 12:34:09 -0700

    implement dnd, room container overflow styling

--- src/app/components/plants/plant.jsx
+++ src/app/components/plants/plant.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import { CardContent, Typography } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
+import { useDrag } from 'react-dnd';
 
 const Plant = (props) => {
 //   if (props){    
@@ -19,22 +20,31 @@ const Plant = (props) => {
     min_light_lux,
     image_url
   } = details;
+
+  const [{ isDragging }, dragRef] = useDrag({
+    type: 'pid',
+    item: { pid, 
+      watering_frequency_per_week, 
+      min_light_lux,
+      image_url
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
   return(
-    <Card sx={{ minWidth: 275, maxWidth: 300, overflowY: scroll, borderRadius: 2 }}>
+    <div ref={dragRef}>
+    <Card sx={{ minHeight: 275, minWidth: 100, maxWidth: 300, overflowY: scroll, borderRadius: 2 }} >
       <CardMedia component='img' image={image_url} sx={{maxHeight: 150}}/>
       <CardContent>
       <Typography variant="subtitle1" gutterBottom> {pid} </Typography>
       <Typography variant="body2" color="text.secondary">Light needed (lux): {min_light_lux}</Typography>
       <Typography variant="body2" color="text.secondary">Watering frequency per week: {watering_frequency_per_week}</Typography>
-        
       </CardContent>
+      {isDragging}
     </Card> 
-    // <div className='plat'>
-    //   <p className="species">Species: {pid}</p>
-    //   <img className= "plant-img" src={image_url} ></img>
-    //   <p>Water {watering_frequency_per_week} x per week</p>
-    //   <p>Light: {min_light_lux}/10</p>
-    // </div>
+    </div>
   );
 }
 
--- src/app/components/room/Room.jsx
+++ src/app/components/room/Room.jsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import './roomContainerStyle.scss';
 import Plant from '../plants/plant.jsx';
 import Card from '@mui/material/Card';
 import { CardContent, Container, Stack } from '@mui/material';
+import { useDrop } from 'react-dnd';
 
 
 
-export default function Room() {
+export default function Room(props) {
+  const { roomPlants, updateRoomPlants, selectedPlants } = props;
+  const [{ isOver }, dropRef] = useDrop({
+    accept: 'pid',
+    drop: (item) => {
+      console.log('drop item: ', item);
+      addPlantToBoard(item.pid)
+      // updateRoomPlants((roomPlants) => !roomPlants.includes(item) ? [...roomPlants, item] : roomPlants)
+    },
+    collect: (monitor) => ({
+        isOver: monitor.isOver()
+    })
+});
+
+  const addPlantToBoard = id => {
+    const plantList = selectedPlants.filter(plant => id === plant.pid)
+    updateRoomPlants((roomPlants) => {
+      const newState = [...roomPlants, plantList[0]];
+      console.log("updated state", newState);
+      return newState;
+    }
+      )
+  }
   
+  useEffect(()=>{
+    console.log('isOver: ', isOver);
+  },[isOver]);
+
+  // console.log("roomPlant", roomPlants);
+  // const plantsInRoom = []
+  // roomPlants.forEach(plant => {
+  //  plantsInRoom.push(<Plant key={plant.pid} draggable details={plant} />)  
+  // })
+  
+  const makeRoomArr = (roomPlants) => {
+    let displayInRoom = roomPlants.map(plant => {
+       console.log('current plant:', plant);
+       return <Plant key={plant.pid} draggable details={plant} />;
+    });
+    return displayInRoom;
+ }
+ const roomArr = makeRoomArr(roomPlants);
+
   return (
-    <Container maxWidth="md">
-      <Stack direction="column" spacing={3}>
-        DRAG HERE
-        <Card>
-          <CardContent>
-            HELLO
-          </CardContent>
-        </Card>
+    <Container sx={{minHeight: "400px", bgcolor: '#d1cdcf', borderRadius: 4, boxShadow: 2}} >
+    DRAG HERE
+    {/* <div ref={dropRef} > */}
+      <Stack direction="row" minHeight="200px" spacing={3} 
+      sx={{overflowX: 'auto', overflowY: 'auto'}}
+      useFlexGap flexWrap="wrap"
+      ref={dropRef} 
+      >
+        {roomArr}
+        {isOver && <div>Drop here</div>}
       </Stack>
+      {/* </div> */}
     </Container>
   );
 }
